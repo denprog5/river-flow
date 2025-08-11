09d5d5ce0b6c660e7c874d194a7e4f226b50b483
denis <denisprogressive@gmail.com>
2025-08-11 13:45:20 +0200

    chore: scaffold with testing, static analysis, and CI/CD baked in

---
+++ src/Version.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denprog\RiverFlow;
+
+/**
+ * Library version information.
+ */
+final class Version
+{
+    public const string STRING = '0.1.0-dev';
+
+    private function __construct() {}
+}
